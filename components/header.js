39e170160622aa81f53a42409ebea73adaee8455
Kirill Vanyukov <vanyukovkirill@gmail.com>
2021-02-24 23:01:56 +0300

    header

--- components/header.js
+++ components/header.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Header({children}) {
-    return (
-        <>
-            <Head>
-                <title>Словарь буддийской терминологии</title>
-                <link rel="shortcut icon" href="favicon.ico"/>
-            </Head>
-            <header>
-
-            </header>
-            <main>
+    return (<>
+        <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <title>Словарь буддийской терминологии</title>
+            <link rel="shortcut icon" href="/favicon.ico"/>
+        </Head>
+        <header>
+            <nav>
+                <Link href="/">
+                    Главная
+                </Link>
+            </nav>
+        </header>
+        <main>
+            <div className="container">
                 {children}
-            </main>
-        </>
+            </div>
+        </main>
+    </>
     )
 }
