e6e98940c9c7b1a542c33720b8d60834d9ffa041
Kirill Vanyukov <vanyukovkirill@gmail.com>
2021-02-26 16:58:25 +0300

    Head init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.scss'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+      <title>Словарь буддийской терминологии</title>
+      <link rel="shortcut icon" href="/favicon.ico"/>
+    </Head>
+    <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Header from "@/components/header";
 
 export default function Home() {
   return (
-      <Header>
+      <>
           <h1> page </h1>
-      </Header>
+      </>
   )
 }
