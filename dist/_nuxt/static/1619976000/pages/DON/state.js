8bb3c2be236ddf43ea4e7faf478b534d6e11816d
Anton Mikhailov <anton@mikhailov.center>
2021-05-02 20:21:03 +0300

    regen

---
+++ dist/_nuxt/static/1619976000/pages/DON/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619976000",layout:"error",error:{statusCode:404,message:"Request failed with status code 500"},serverRendered:false,routePath:"\u002Fpages\u002FDON",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}};
