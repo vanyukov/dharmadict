add6a73105969f5b6658901f3fab7fa5e2a41942
turrandott <tarzhakova@gmail.com>
2021-05-20 12:49:00 +0200

    Translator's page

---
+++ components/AboutTranslator.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="flex flex-col items-center">
+        <img 
+            :src="translator.img" 
+            class="h-48 w-48 rounded-full shadow-md m-6"
+        />
+        <div class="shadow-md rounded-md p-4 py-8 m-6 w-full html-content" v-html="about.content">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        translator: {
+            type: Object,
+            required: true
+        },
+        about: {
+            type: Object,
+            required: false
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .html-content::v-deep h2 {
+        margin-top: 4rem;
+        margin-bottom: 2rem;
+        font-size: 1.25rem;
+    }
+    .html-content::v-deep p {
+        margin-top: 1rem;
+        margin-bottom: 1rem; 
+    }
+    .html-content::v-deep ul {
+        list-style-type: disc;
+        margin-left: 1rem;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -14,16 +14,22 @@
     </div>
 
 		<ul class="container mb-2 mx-auto w-full my-4 md:my-12 flex flex-col md:flex-row flex-wrap p-4 items-stretch">
-      <li class="m-3 flex-1 h-auto select-none h-full items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:shadow-lg rounded-2xl border-2 p-6 border-green-500 text-center text-xl">
-        <NuxtLink to="/pages/parallel_texts">
-          Параллельные тексты
-        </NuxtLink>
-      </li>
-      <li class="m-3 flex-1 h-auto select-none h-full items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:shadow-lg rounded-2xl border-2 p-6 border-green-500 text-center text-xl">
-        <NuxtLink to="/pages/translators">
+      <NuxtLink 
+        to="/pages/parallel_texts"
+        class="m-3 flex-1 h-auto select-none h-full items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:shadow-lg rounded-2xl border-2 p-6 border-green-500 text-center text-xl"
+      >
+        <li class="h-auto select-none h-full items-center text-center text-xl">
+            Параллельные тексты
+        </li>
+      </NuxtLink>
+      <NuxtLink
+        to="/pages/translators"
+        class="m-3 flex-1 h-auto select-none h-full items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:shadow-lg rounded-2xl border-2 p-6 border-green-500 text-center text-xl"
+      >
+        <li class="h-auto select-none h-full items-center text-center text-xl">
           Переводчики
-        </NuxtLink>
-      </li>
+        </li>
+      </NuxtLink>
     </ul>
 
     <p v-if="$fetchState.pending">
