bbeb754b8b3127c10c9abd2a7e33dbec7d0e607a
Kirill Vanyukov <vanyukovkirill@gmail.com>
2021-07-02 20:04:51 +0300

    Home. prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
     <div
-      class="container mb-2 mx-auto px-8 w-full mt-4 md:mt-12 flex flex-wrap justify-evenly"
+      class="
+        container
+        mb-2
+        mx-auto
+        px-8
+        w-full
+        mt-4
+        md:mt-12
+        flex flex-wrap
+        justify-evenly
+      "
     >
       <div class="flex-initial w-2/3 md:w-1/3 flex">
-        <img src="/images/manj.png" id="image" alt="logo" class="w-full h-full"/>
+        <img
+          src="/images/manj.png"
+          id="image"
+          alt="logo"
+          class="w-full h-full"
+        />
       </div>
-      <div class="flex-2 flex flex-col justify-center w-full md:w-2/3 py-6 md:pl-9">
+      <div
+        class="flex-2 flex flex-col justify-center w-full md:w-2/3 py-6 md:pl-9"
+      >
         <h1
-          class="text-4xl md:text-6xl lg:text-7xl text-green-500 text-center md:text-left"
+          class="
+            text-4xl
+            md:text-6xl
+            lg:text-7xl
+            text-green-500 text-center
+            md:text-left
+          "
         >
           <strong>Буддийская терминология</strong>
           <br />
@@ -18,17 +41,69 @@
     </div>
 
     <div
-      class="container mb-2 mx-auto w-full my-4 md:my-12 flex flex-col md:flex-row flex-wrap p-4 items-stretch"
+      class="
+        container
+        mb-2
+        mx-auto
+        w-full
+        my-4
+        md:my-12
+        flex flex-col
+        md:flex-row
+        flex-wrap
+        p-4
+        items-stretch
+      "
     >
       <NuxtLink
         to="/pages/parallel_texts"
-        class="m-3 flex-1 h-auto select-none h-full items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:shadow-lg rounded-2xl border-2 p-6 border-green-500 text-center text-xl no-underline"
+        class="
+          m-3
+          flex-1
+          h-auto
+          select-none
+          h-full
+          items-center
+          p-4
+          transition
+          duration-500
+          ease-in-out
+          transform
+          hover:-translate-y-0.5
+          hover:shadow-lg
+          rounded-2xl
+          border-2
+          p-6
+          border-green-500
+          text-center text-xl
+          no-underline
+        "
       >
         Параллельные тексты
       </NuxtLink>
       <NuxtLink
         to="/pages/translators"
-        class="m-3 flex-1 h-auto select-none h-full items-center p-4 transition duration-500 ease-in-out transform hover:-translate-y-0.5 hover:shadow-lg rounded-2xl border-2 p-6 border-green-500 text-center text-xl no-underline"
+        class="
+          m-3
+          flex-1
+          h-auto
+          select-none
+          h-full
+          items-center
+          p-4
+          transition
+          duration-500
+          ease-in-out
+          transform
+          hover:-translate-y-0.5
+          hover:shadow-lg
+          rounded-2xl
+          border-2
+          p-6
+          border-green-500
+          text-center text-xl
+          no-underline
+        "
       >
         Переводчики
       </NuxtLink>
@@ -46,7 +121,19 @@
         class="group w-full cursor-pointer my-6"
       >
         <NuxtLink :to="'/' + item.url">
-          <h2 class="w-full text-2xl transition duration-500 ease-in-out transform group-hover:translate-x-3 my-1 no-underline">
+          <h2
+            class="
+              w-full
+              text-2xl
+              transition
+              duration-500
+              ease-in-out
+              transform
+              group-hover:translate-x-3
+              my-1
+              no-underline
+            "
+          >
             {{ item.title.trim() }}
           </h2>
           <p class="w-full text-sm text-gray-500 no-underline">
